4dab1fa3883d789fcd65de2f48083122d756acd3
Jenil-Vekaria <jvekaria@ryerson.ca>
2022-08-16 22:49:46 -0400

    Client - Add login/signup and connect to backend JWT

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { Auth } from "./pages/Auth";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Auth />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/services/auth-service.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+
+const API_URL = process.env.REACT_APP_API_ENDPOINT + "/auth";
+
+const signup = (user) => {
+    /*
+        @param - user
+        firstName
+        lastName
+        email
+        password
+        confirmPassword
+    */
+    return axios.post(API_URL + "/signup", user)
+        .then((response) => {
+            if (response.data.accessToken) {
+                localStorage.setItem("user", JSON.stringify(response.data));
+            }
+
+            return response.data;
+        });
+};
+
+const login = (user) => {
+    /*
+        @param - user
+        email
+        password
+    */
+
+    return axios.post(API_URL + "/login", user)
+        .then((response) => {
+            if (response.data.accessToken) {
+                localStorage.setItem("user", JSON.stringify(response.data));
+            }
+
+            return response.data;
+        });
+};
+
+const logout = () => {
+    localStorage.removeItem("user");
+};
+
+const getCurrentUser = () => {
+    return JSON.parse(localStorage.getItem("user"));
+};
+
+const authService = {
+    signup,
+    login,
+    logout,
+    getCurrentUser
+};
+
+export default authService;
