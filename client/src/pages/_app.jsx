96b10fec4cee53997a312ed012357aafab32f309
Jenil-Vekaria <jenilvekaria@outlook.com>
2023-11-03 23:46:39 -0400

    fix: importing react data grid css file in the app.jsx rather than the table component itself

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -1,5 +1,6 @@
 import { theme } from "@/styles/theme.js";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/provider";
+import "@inovua/reactdatagrid-community/index.css";
 import "react-quill/dist/quill.snow.css";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
