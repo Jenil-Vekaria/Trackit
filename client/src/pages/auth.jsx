ccd583c853aa89475a61b26993f2a8cf395433f7
Jenil-Vekaria <jenilvekaria@outlook.com>
2023-11-03 23:45:45 -0400

    fix: sperate demo login info into another component

--- client/src/pages/auth.jsx
+++ client/src/pages/auth.jsx
@@ -1,38 +1,19 @@
 import Image from "next/image";
 import {
   Button,
-  HStack,
   Heading,
   Icon,
   Link,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  Table,
-  TableContainer,
-  Tbody,
-  Td,
-  Text,
-  Th,
-  Thead,
-  Tr,
   VStack,
   useDisclosure,
 } from "@chakra-ui/react";
-import { useState } from "react";
 import { AiFillGithub } from "react-icons/ai";
 import { Login } from "@/components/authentication/Login";
-import { SignUp } from "@/components/authentication/SignUp";
-import { DEMO_LOGIN_INFO } from "@/util/Constants";
+import DemoLoginInfoModal from "@/components/others/DemoLoginInfoModal";
 import logo from "@/assets/Trackit_Plain.png";
 
 export const Auth = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [isLogin, setisLogin] = useState(true);
 
   return (
     <>
@@ -51,41 +32,7 @@ export const Auth = () => {
         <Login />
         <br />
         <Button onClick={onOpen}>Demo Login Info</Button>
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Demo Login Info</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <TableContainer>
-                <Table variant="simple">
-                  <Thead>
-                    <Tr>
-                      <Th>Email</Th>
-                      <Th>Password</Th>
-                      <Th>Role</Th>
-                    </Tr>
-                  </Thead>
-                  <Tbody>
-                    {DEMO_LOGIN_INFO.map((loginInfo, index) => (
-                      <Tr key={index}>
-                        <Td>{loginInfo.email}</Td>
-                        <Td>{loginInfo.password}</Td>
-                        <Td>{loginInfo.role}</Td>
-                      </Tr>
-                    ))}
-                  </Tbody>
-                </Table>
-              </TableContainer>
-            </ModalBody>
-
-            <ModalFooter>
-              <Button colorScheme="blue" mr={3} onClick={onClose}>
-                Close
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
+        <DemoLoginInfoModal isOpen={isOpen} onClose={onClose} />
       </VStack>
     </>
   );
